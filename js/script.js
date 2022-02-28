12bd9ea1f8df0d528d5e0ad691dc5134a77a34e3
Rezaul909 <reza1381996@gmail.com>
2022-03-01 01:24:00 +0600

    card-added

---
+++ js/script.js
@@ -0,0 +1,79 @@
+const loadData= () =>{
+    const inputField = document.getElementById("input-field");
+    const inputValue = inputField.value;
+
+    const url = `https://openapi.programming-hero.com/api/phones?search=${inputValue}`;
+    fetch(url)
+    .then(response => response.json())
+    .then(data => displayPhone(data.data))
+
+    inputField.value = "";
+}
+
+const displayPhone = phones => {
+    // console.log(phones);
+    for(const phone of phones){
+        // console.log(phone);
+        const parentDiv = document.getElementById("card-container");
+        const div = document.createElement("div");
+        div.classList.add('col')
+        div.innerHTML = `
+            <div class="card h-100">
+                <img src="${phone.image}" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h4 class="card-title">Model: ${phone.phone_name}</h4>
+                    <p class="card-text">Brand: ${phone.brand}</p>
+                </div>
+                <div class="card-footer">
+                    <a onClick="detailsPhone('${phone.slug}')" href="#" class="btn btn-primary">See Details</a>
+                </div>
+            </div>
+        `;
+        parentDiv.appendChild(div);
+    }
+}
+
+const detailsPhone = phoneId =>{
+    const url = `https://openapi.programming-hero.com/api/phone/${phoneId}`;
+    fetch(url)
+    .then(response => response.json())
+    .then(data => displayDetails(data))
+}
+
+const displayDetails = phoneId =>{
+    console.log(phoneId);
+    // console.log(phoneId.data.mainFeatures.sensors);
+    const sensors = phoneId.data.mainFeatures.sensors;
+    for(const sensor of sensors){
+        console.log(sensor);
+    }
+    const phoneDetails = document.getElementById("phone-details");
+    const div = document.createElement("div");
+    div.innerHTML = `
+        <div class="card mx-auto mb-5" style="width: 18rem;">
+            <img src="${phoneId.data.image}" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h4 class="card-title">Model: ${phoneId.data.name}</h4>
+                <p class="fst-italic">${phoneId.data.releaseDate}</p>
+                <p class="card-text">Brand: ${phoneId.data.brand} </p>
+                <h5 class="text-info text-decoration-underline">Main Features:</h5>
+                <p>
+                    <span class="fw-bolder">Chip Set: </span>${phoneId.data.mainFeatures.chipSet}<hr>
+                    <span class="fw-bolder">Display: </span>${phoneId.data.mainFeatures.displaySize}<hr>
+                    <span class="fw-bolder">Memory: </span>${phoneId.data.mainFeatures.memory}<hr>
+                    <span class="fw-bolder">Storage: </span>${phoneId.data.mainFeatures.storage}<hr>
+                </p>
+                <h5 class="text-info text-decoration-underline">Others:</h5>
+                <p>
+                    <span class="fw-bolder">Bluetooth: </span>${phoneId.data.others.Bluetooth}<hr>
+                    <span class="fw-bolder">GPS: </span>${phoneId.data.others.GPS}<hr>
+                    <span class="fw-bolder">NFC: </span>${phoneId.data.others.NFC}<hr>
+                    <span class="fw-bolder">Radio: </span>${phoneId.data.others.Radio}<hr>
+                    <span class="fw-bolder">USB: </span>${phoneId.data.others.USB}<hr>
+                    <span class="fw-bolder">WLAN: </span>${phoneId.data.others.WLAN}<hr>
+                </p>
+            </div>
+        </div>
+    `;
+    phoneDetails.appendChild(div);
+}
